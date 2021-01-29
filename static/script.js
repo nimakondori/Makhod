ee9eae9179f28c0d70857f6eb04865bbbaca4580
Nima <nimakondori96@gmail.com>
2021-01-29 00:19:16 -0800

    added detection of anniversary

--- static/script.js
+++ static/script.js
@@ -10,6 +10,14 @@ function Countdown() {
         document.getElementById('hours').innerHTML = data.hour;
         document.getElementById('minutes').innerHTML = data.min;
         document.getElementById('secs').innerHTML = data.sec;
+        if (data.day == 0 && data.hour == 0 && data.min == 0 && data.sec==0){
+            document.getElementById('days').innerHTML = ""
+            document.getElementById('hours').innerHTML = ""
+            document.getElementById('minutes').innerHTML = ""
+            document.getElementById('secs').innerHTML = ""
+            document.getElementById("button").style.display = "inline-block"
+        }
+
     };
 };
 window.onload = function() {
