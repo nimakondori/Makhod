e90f0c6f7e781fd87ed6f9d070cdc33b4c4d606e
Nima <nimakondori96@gmail.com>
2021-01-31 22:46:52 -0800

    added google

--- cli.py
+++ cli.py
@@ -1,10 +1,10 @@
 import os
 import time
 
-from utils import count_down, india_zone
+from utils import count_down, zone
 
 while True:
     os.system("clear")
-    print("{} Days {} Hours {} Minutes and {} Seconds".format(*count_down(india_zone)))
+    print("{} Days {} Hours {} Minutes and {} Seconds".format(*count_down(zone)))
     time.sleep(1)
     os.system("clear")
